c2db5c3dd965896924898143d5ec81f5fbc18307
cassievg <cassievalleriagarcia@gmail.com>
2026-06-04 23:06:43 +0700

    data rendering fix

--- src/context/UserDataContext.js
+++ src/context/UserDataContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { renderUserData } from '../database/databaseServices';
 import { useAuth } from './AuthContext';
 import { useDatabase } from './DBContext';
@@ -10,20 +10,42 @@ const useUserData = () => useContext(UserDataContext);
 const UserDataProvider = ({ children }) => {
     const { currentUser, userId } = useAuth();
     const { db } = useDatabase();
+
+    const [userData, setUserData] = useState([]);
     
     const [progress, setProgress] = useState([]);    
     const [cache, setCache] = useState([]);    
     const [results, setResults] = useState([]);
 
     const loadUser = async () => {
-        if (!currentUser || !userId) return;
-
         const data = await renderUserData(db, userId);
-
-        setProgress(data.userProgress);
-        setCache(data.userCache);
-        setResults(data.userResults);
     };
+
+    useEffect(() => {
+        const loadUser = async () => {
+            return await renderUserData(db, userId);
+        };
+
+        const init = async () => {
+            let user;
+
+            if (userId) {
+                user = await loadUser();
+                setProgress(user.userProgress);
+                setCache(user.userCache);
+                setResults(user.userResults);
+                setUserData(user);
+            }
+            else {
+                setProgress(null);
+                setCache(null);
+                setResults(null);
+                setUserData(null);
+            }
+        };
+
+        init();
+    }, [userId])
 
     return (
         <UserDataContext.Provider value={{progress, setProgress, cache, setCache, results, setResults, loadUser}}>
